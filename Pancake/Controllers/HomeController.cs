32c0417ac3e969a2fafda7fae6b3c3d51d83f5f5
Owl <likmoon@live.ru>
2016-12-03 23:45:17 +0300

    day1

--- Pancake/Controllers/HomeController.cs
+++ Pancake/Controllers/HomeController.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace Pancake.Controllers
 {
+    
     public class HomeController : Controller
     {
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult About(String queueMessage)
         {
-            ViewBag.Message = "Your application description page.";
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            // Retrieve a reference to a container.
+            CloudQueue queue = queueClient.GetQueueReference("pancakequeue");
+
+            // Create the queue if it doesn't already exist
+            queue.CreateIfNotExists();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(queueMessage);
+            queue.AddMessage(message);
+            // Peek at the next message
+            CloudQueueMessage peekedMessage = queue.PeekMessage();
+
+            ViewBag.Message = String.Format("Ваше сообщение добавлено: {0}", peekedMessage.AsString);
 
             return View();
         }
